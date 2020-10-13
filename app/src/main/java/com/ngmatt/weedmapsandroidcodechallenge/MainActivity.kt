359f0dda63d9fd3b2ab5511e75535e39846c7c3b
Dominic Cobb <dominicshajaa@icloud.com>
2020-10-12 18:18:28 -0700

    learned radio and checkboxes

--- app/src/main/java/com/ngmatt/weedmapsandroidcodechallenge/MainActivity.kt
+++ app/src/main/java/com/ngmatt/weedmapsandroidcodechallenge/MainActivity.kt
@@ -2,17 +2,30 @@ package com.ngmatt.weedmapsandroidcodechallenge
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
 
-/**
- * Created by Matt Ng on 9/14/20
- */
-class MainActivity: Activity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
+        btnOrder.setOnClickListener {
+            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+            val cheese = cbCheese.isChecked
+            val onions = cbOnions.isChecked
+            val salad = cbSalad.isChecked
+            val orderString = "You ordered a burger with:\n" +
+                    "${meat.text}" +
+                    (if(cheese) "\nCheese" else "") +
+                    (if(onions) "\nOnions" else "") +
+                    (if(salad) "\nSalad" else "")
+            tvOrder.text = orderString
+        }
     }
+
+
 }
